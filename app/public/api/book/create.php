7c2ae8598ece85e273e11cd376107e5d168fb1b4
eclashley <emcathawk2000@gmail.com>
2021-10-20 10:20:11 -0400

    redirect

--- app/public/api/book/create.php
+++ app/public/api/book/create.php
@@ -32,12 +32,11 @@ $db = DbConnection::getConnection();
 // Step 2: Create & run the query
 // Note the use of parameterized statements to avoid injection
 $stmt = $db->prepare(
-  'INSERT INTO book (id, title, author, yr_published, publisher , pg_ct, msrp )
-  VALUES (?, ?, ?, ?, ?, ?, ?)'
+  'INSERT INTO book (title, author, yr_published, publisher , pg_ct, msrp )
+  VALUES (?, ?, ?, ?, ?, ?)'
 );
 
 $stmt->execute([
-  $_POST['id'],
   $_POST['title'],
   $_POST['author'],
   $_POST['yr_published'],
@@ -45,14 +44,11 @@ $stmt->execute([
   $_POST['pg_ct'],
   $_POST['msrp']
 ]);
-
-// Get auto-generated PK from DB
-// https://www.php.net/manual/en/pdo.lastinsertid.php
-$pk = $db->lastInsertId();  
+ 
 
 // Step 4: Output
 // Here, instead of giving output, I'm redirecting to the SELECT API,
 // just in case the data changed by entering it
 header('HTTP/1.1 303 See Other');
-header('Location: ../book/create.php');
+header('Location: ../book');
 
